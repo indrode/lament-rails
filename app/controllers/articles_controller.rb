46005f36d244418a268e933286c4dc7aa35d42a4
Indro De <indro.de@gmail.com>
2015-03-28 01:31:48 +0100

    Adjust articles backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,11 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PW'], except: %i(index show)
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PW']
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
   # GET /articles.json
   def index
     @articles = Article.all
-  end
-
-  # GET /articles/1
-  # GET /articles/1.json
-  def show
   end
 
   # GET /articles/new
@@ -26,39 +21,18 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to articles_url, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to articles_url, notice: 'Article was successfully created'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to articles_url, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /articles/1
-  # DELETE /articles/1.json
-  def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
+    if @article.update(article_params)
+      redirect_to articles_url, notice: 'Article was successfully updated'
+    else
+      render :edit
     end
   end
 
