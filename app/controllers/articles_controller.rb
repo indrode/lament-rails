db125d262d2b0a60fe35a6f7bdb888af962460cc
Indro De <indro.de@gmail.com>
2015-03-31 01:34:33 +0200

    Adjust admin interface controller and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,23 +2,17 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PW']
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
   def index
     @articles = Article.all
   end
 
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -45,6 +39,6 @@ class ArticlesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def article_params
-    params.require(:article).permit(:enabled, :number, :title, :blurb, :filename, :category, :copy, :posted_at)
+    params.require(:article).permit(:enabled, :number, :title, :blurb, :category, :copy, :posted_at)
   end
 end
