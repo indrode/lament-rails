2e30bc0ae8b06c238435fe028786d9eed9635c12
Indro De <indro.de@gmail.com>
2015-04-12 03:04:45 +0200

    Adjust puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,42 +1,12 @@
-#!/usr/bin/env puma
-
-# app do |env|
-#   puts env
-#
-#   body = 'Hello, World!'
-#
-#   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
-# end
-
 environment 'production'
-daemonize false
+daemonize true
 
 pidfile '/home/akira/lament-rails/shared/tmp/pids/puma.pid'
 state_path '/home/akira/lament-rails/shared/tmp/pids/puma.state'
 
-# stdout_redirect 'log/puma.log', 'log/puma_err.log'
+threads 2, 4
+bind 'unix:///home/akira/lament-rails/shared/tmp/sockets/puma.sock'
 
-# quiet
-threads 0, 16
-bind 'unix://tmp/sockets/puma.sock'
-
-# ssl_bind '127.0.0.1', '9292', { key: path_to_key, cert: path_to_cert }
-
-# on_restart do
-#   puts 'On restart...'
-# end
-
-# restart_command '/u/app/lolcat/bin/restart_puma'
-
-
-# === Cluster mode ===
-
-# workers 2
-
-# on_worker_boot do
-#   puts 'On worker boot...'
-# end
-
-# === Puma control rack application ===
-
-activate_control_app 'unix://tmp/sockets/pumactl.sock'
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
