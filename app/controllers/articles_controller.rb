6d28c4c6b10ba16b9738c469b4501bbd1fd0c997
Indro De <indro.de@gmail.com>
2015-06-06 14:53:38 +0200

    Change admin articles order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.ordered.all
   end
 
   def new
