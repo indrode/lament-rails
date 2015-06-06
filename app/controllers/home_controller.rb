cc2c20daeba2826d20435417b37dd8931c2ef14d
Indro De <indro.de@gmail.com>
2015-06-06 15:23:12 +0200

    Find articles by number and rescue 404s

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,17 @@
 class HomeController < ApplicationController
+  before_filter :load_article, only: :show
+
   def index
     @article = Article.enabled.ordered.first
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def load_article
+    @article = Article.find_by_number(params[:number])
+    render_404 unless @article
   end
 end
