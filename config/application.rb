8b8b34ae24f67c776df9d6d2baacfd3593f56101
Indro De <indro.de@gmail.com>
2015-06-14 02:22:44 +0200

    Do not generate controller-specific assets

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module LamentRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets false
+    end
   end
 end
