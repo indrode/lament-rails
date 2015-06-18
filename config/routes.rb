4f1c1002b7e5728f960a1e1e97e10424ee63f6f9
Indro De <indro.de@gmail.com>
2015-06-18 12:26:18 +0200

    Add categories#show pages and refactor views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   root 'home#index'
   get ':number' => 'home#show', as: :blog
+  get 'categories/:title' => 'categories#show', as: :category
 end
