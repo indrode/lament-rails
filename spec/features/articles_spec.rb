55f3b05d48f9e148e2bc18a70a1991a382be2643
Indro De <indro.de@gmail.com>
2015-06-21 23:55:32 +0200

    Add articles#new and #edit specs

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -8,7 +8,10 @@ def visit_with_basic_auth(path)
 end
 
 RSpec.describe ArticlesController, type: :feature do
-  before { create(:article, number: 1, posted_at: Date.new(2015, 4, 20)) }
+  before do
+    create(:article, number: 1, posted_at: Date.new(2015, 4, 20))
+    create(:category, title: 'CategoryA')
+  end
 
   describe 'Admin Interface' do
     it 'should display correctly' do
@@ -44,10 +47,33 @@ RSpec.describe ArticlesController, type: :feature do
       expect(page).to have_content 'New Article'
       expect(page).to have_content 'Please review the problems below:'
     end
+
+    it 'should successfully create a new article' do
+      visit_with_basic_auth articles_path
+      click_link 'Add a new article'
+      select('Yes', from: 'Enabled')
+      fill_in('Number', with: '23')
+      select('CategoryA', from: 'Category')
+      fill_in('Title', with: 'Title')
+      fill_in('Blurb', with: 'Blurb')
+      fill_in('Markdown', with: 'Markdown')
+      click_button 'Create Article'
+      expect(page).to have_content 'Article was successfully created'
+    end
   end
 
   describe 'Editing an existing article' do
     it 'should display validation messages' do
+      visit_with_basic_auth articles_path
+      click_link 'Article Title'
+      expect(page).to have_content 'Edit Article'
+      fill_in('Title', with: '')
+      click_button 'Update Article'
+      expect(page).to have_content 'Edit Article'
+      expect(page).to have_content 'Please review the problems below:'
+    end
+
+    it 'should successfully edit an existing article' do
       visit_with_basic_auth articles_path
       click_link 'Article Title'
       expect(page).to have_content 'Edit Article'
