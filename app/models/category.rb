beae1eb6e3b1a5cd8dfec6324acf9e995aa45b65
Indro De <indro.de@gmail.com>
2015-06-22 15:13:29 +0200

    Add description field to recent articles

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,11 @@ class Category < ActiveRecord::Base
 
   class << self
     def recent(limit = 5)
-      OpenStruct.new(title: 'recently', articles: Article.recent(limit))
+      OpenStruct.new(
+        title: 'recently',
+        description: 'The most recently posted articles.',
+        articles: Article.recent(limit)
+      )
     end
   end
 end
