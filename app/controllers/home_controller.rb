faec44ce8e77cc1de3652bab90adff811e40ade9
Indro De <indro.de@gmail.com>
2015-06-23 00:56:07 +0200

    Start implementing search feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   caches_page :index, :show
+  before_filter :set_query
 
   def index
     @categories = Category.includes(:articles).all
@@ -8,5 +9,15 @@ class HomeController < ApplicationController
 
   def show
     @article = Article.find_by_number(params[:number])
+  end
+
+  def results
+    @results = Article.search(@query)
+  end
+
+  private
+
+  def set_query
+    @query = params[:query] || params[:highlight]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
   root 'home#index'
+  get 'results' => 'home#results', as: :results
   get ':number' => 'home#show', as: :blog
   get 'categories/:title' => 'categories#show', as: :category
 end
