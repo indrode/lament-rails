6875541a547b502ee20a7b72b59db2fe64f691a3
Indro De <indro.de@gmail.com>
2015-06-23 21:16:20 +0200

    Implement Feature logic

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,33 @@
 class Feature < ActiveRecord::Base
+  FEATURES = {
+    search: false
+  }
+
+  class << self
+    FEATURES.each do |key, _|
+      define_method("#{key}_enabled?") do
+        Feature.find_by_key(key).enabled
+      end
+    end
+
+    def reload_features!
+      FEATURES.each do |key, value|
+        self.create_with(key: key, enabled: value).find_or_create_by(key: key)
+      end
+    end
+
+    def enable!(key)
+      if FEATURES.has_key?(key.to_sym)
+        Feature.find_by_key(key).update_attributes(enabled: true)
+      end
+    end
+
+    def disable!(key)
+      if FEATURES.has_key?(key.to_sym)
+        Feature.find_by_key(key).update_attributes(enabled: false)
+      end
+    end
+  end
+
+  reload_features!
 end
--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Feature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'class methods' do
+    before do
+      stub_const('Feature::FEATURES', { search: false, authentication: true })
+      Feature.reload_features!
+    end
+
+    describe '.reload_features!' do
+      it 'stores defined features' do
+        expect(Feature.count).to eq(2)
+        expect(Feature.last.key).to eq('authentication')
+      end
+    end
+
+    describe '.key_enabled?' do
+      it 'creates enabled? methods' do
+        expect(Feature.search_enabled?).to be_falsey
+      end
+    end
+
+    describe '.enable!' do
+      it 'enables a feature' do
+        expect(Feature.search_enabled?).to be_falsey
+        Feature.enable!(:search)
+        expect(Feature.search_enabled?).to be_truthy
+      end
+    end
+
+    describe '.disable!' do
+      it 'disables a feature' do
+        Feature.enable!(:search)
+        expect(Feature.search_enabled?).to be_truthy
+        Feature.disable!(:search)
+        expect(Feature.search_enabled?).to be_falsey
+      end
+    end
+  end
 end
